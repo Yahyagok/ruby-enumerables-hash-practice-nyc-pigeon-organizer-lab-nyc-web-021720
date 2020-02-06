6e7bd6e662ff322273796713033f08b42fbe106f
Yahya  Gok <ygok17@hotmail.com>
2020-02-06 20:06:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,31 +1,71 @@
-def nyc_pigeon_organizer(data)
+# def nyc_pigeon_organizer(data)
   
-  hash = {}
+#   hash = {}
   
-  data.each do |first_level, second_level| 
-    second_level.each do |category, names|
-      names.each do |name|
-        hash[name] = {:color => [] , :gender => [], :lives =>[]}
+#   data.each do |first_level, second_level| 
+#     second_level.each do |category, names|
+#       names.each do |name|
+#         hash[name] = {:color => [] , :gender => [], :lives =>[]}
+#       end 
+#     end 
+#   end 
+#   x = hash.keys 
+#   data[:color].each do | bird_color, names|
+#     names.each do |name|
+#       x.each do |item|
+#         if name == item 
+#           hash[item][:color] << bird_color.to_s
+    
+#   end
+# end
+# end
+# end 
+  
+  
+  
+# end
+
+def nyc_pigeon_organizer (data)
+  final = {}
+
+  data.each do |first_level, all_other|
+    all_other.each do |category, array|
+      array.each do |name|
+        final[name] = {:color => [], :gender => [], :lives => []}
       end 
     end 
   end 
-  x = hash.keys 
-  data[:color].each do | bird_color, names|
-    names.each do |name|
+  x = final.keys
+  data[:color].each do |bird_color, name|
+    name.each do |bird_name|
       x.each do |item|
-        if name == item 
-          hash[item][:color] << bird_color.to_s
-    
-  end
-end
-end
+        if bird_name === item
+          final[item][:color] << bird_color.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:gender].each do |gender, type|
+    type.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:gender] << gender.to_s
+        end 
+      end 
+    end 
+  end 
+  data[:lives].each do |location, name|
+    name.each do |bird_name|
+      x.each do |item|
+        if bird_name === item
+          final[item][:lives] << location
+        end 
+      end 
+    end 
+  end 
+  
+  return final 
 end 
-  
-  
-  
-end
-
-
 
 
 
