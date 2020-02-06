833bb589e6ea3a0dd4e17d857b0b7bf83f76a3c8
Yahya  Gok <ygok17@hotmail.com>
2020-02-06 19:37:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  hash = {}
+  
+  data.each do |first_level, second_level| 
+    second_level.each do |category, names|
+      names.each do |name|
+        hash[name] = {:color => [] , :gender => [], :lives =>[]}
+      end 
+    end 
+  end 
+  
+  
+  
+  
+  
 end
